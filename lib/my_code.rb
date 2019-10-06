9db1ff3e1a8dd6751462f8fe25f9545c20c11574
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-06 05:27:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,13 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-  return source_array
+  new = []
+  i = 0 
+  while i < source_array.length do
+    new.push(source_array[i])
+    i += 1
+  end
+  new
 end
 
 def map_to_double(source_array)
