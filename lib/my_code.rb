30a1f455b05b42e13912f2bc2936465774c6af31
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-06 05:22:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,8 +47,8 @@ def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do
     return false if !source_array[i]
-  else 
-    true
+  end
+  true
 end
 
 def reduce_to_any_true(source_array)
