a9aabea4ace0c2eb1e35d3d412d0401215f83b49
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-06 05:12:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,10 +37,10 @@ def reduce_to_total(source_array, starting_point)
   total = starting_point 
   i = 0
   while i < source_array.length do
-    total += source_array[i]
+    total = total + source_array[i]
     i += 1
   end
-  return total
+  total
 end
 
 def reduce_to_all_true(source_array)
